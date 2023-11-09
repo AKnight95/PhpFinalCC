e384a12f71b302d93f84c0e49f621cb4bb26d2fd
Amine KASSI <amine.kassi@goons.fr>
2023-11-09 21:44:46 +0100

    Update

---
+++ phpFinal/src/Controller/AdminController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use App\Form\ArticlesType;
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/admin', name: 'app_admin')]
+class AdminController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/', name: 'app_articles_index', methods: ['GET'])]
+    public function getAll(ArticlesRepository $articlesRepository): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'articles' => $articlesRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_articles_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $article = new Articles();
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('articles/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_articles_show', methods: ['GET'])]
+    public function show(Articles $article): Response
+    {
+        return $this->render('articles/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_articles_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_articles_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('articles/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_articles_delete', methods: ['POST'])]
+    public function delete(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_articles_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+}
---
+++ phpFinal/src/Entity/Articles.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    #[ORM\Column(type: 'string', length: 255)]
+    private $status = 'WAITING'; // Default value set to "WAITING"
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        if (!in_array($status, ['APPROVED', 'WAITING'])) {
+            throw new \InvalidArgumentException("Invalid status");
+        }
+
+        $this->status = $status;
+
+        return $this;
+    }
+}
